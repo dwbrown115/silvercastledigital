4a86bc4d5859620df4258c184b84b80c1ac8808b
dwbrown115 <dakota.w.brown@silvercastledigital.com>
2022-09-27 03:56:58 -0500

    started work on hero page

--- src/Screens/Hero/index.js
+++ src/Screens/Hero/index.js
@@ -9,11 +9,36 @@ class Hero extends PureComponent {
     this.state = {};
   }
 
+  scrollTo(name) {
+    const element = document.getElementById(name);
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+
   render() {
     return (
       <div className="hero-container">
         <div id="hero" style={{ position: "absolute", right: "50%" }} />
-        <div className="hero">Hero</div>
+        <div className="hero">
+          <div className="hero-image-wrapper">
+            <div className="hero-image-and-text">
+              <div className="hero-header">
+                Let's Level Up Your Buisness With A New Website
+              </div>
+              <div
+                className="contact-button"
+                onClick={() => this.scrollTo("contact")}
+              >
+                Contact Us
+              </div>
+              <div
+                className="learn-more-button"
+                onClick={() => this.scrollTo("about")}
+              >
+                Learn More
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
