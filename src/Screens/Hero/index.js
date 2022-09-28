36bcc5737cac4ac0430b5a2edb0ef6bc2c0eee04
dwbrown115 <dakota.w.brown@silvercastledigital.com>
2022-09-27 20:57:24 -0500

    continued hero page, added css to contact and learn more buttons

--- src/Screens/Hero/index.js
+++ src/Screens/Hero/index.js
@@ -6,12 +6,83 @@ class Hero extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      contactButtonTextColor: "#F5F5F5",
+      contactBgColor: "transparent",
+      contactBorderColor: "#F5F5F5",
+      learnButtonTextColor: "#F5F5F5",
+      learnBgColor: "transparent",
+      learnBorderColor: "#F5F5F5",
+    };
   }
 
   scrollTo(name) {
     const element = document.getElementById(name);
     element.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+
+  contactButtonOver() {
+    if (this.state.status === "moved") {
+      this.setState({
+        contactButtonTextColor: "#F5F5F5",
+        contactBgColor: "#B78080",
+        contactBorderColor: "#B78080",
+      });
+    } else {
+      this.setState({
+        contactButtonTextColor: "#B78080",
+        contactBgColor: "#F5F5F5",
+        contactBorderColor: "#F5F5F5",
+      });
+    }
+  }
+
+  contactButtonLeave() {
+    if (this.state.status === "moved") {
+      this.setState({
+        contactButtonTextColor: "#B78080",
+        contactBgColor: "#F5F5F5",
+        contactBorderColor: "#B78080",
+      });
+    } else {
+      this.setState({
+        contactButtonTextColor: "#F5F5F5",
+        contactBgColor: "transparent",
+        contactBorderColor: "#F5F5F5",
+      });
+    }
+  }
+
+  learnButtonOver() {
+    if (this.state.status === "moved") {
+      this.setState({
+        learnButtonTextColor: "#F5F5F5",
+        learnBgColor: "#B78080",
+        learnBorderColor: "#B78080",
+      });
+    } else {
+      this.setState({
+        learnButtonTextColor: "#B78080",
+        learnBgColor: "#F5F5F5",
+        learnBorderColor: "#F5F5F5",
+      });
+    }
+  }
+
+  learnButtonLeave() {
+    if (this.state.status === "moved") {
+      this.setState({
+        learnButtonTextColor: "#B78080",
+        learnBgColor: "#F5F5F5",
+        learnBorderColor: "#B78080",
+      });
+    } else {
+      this.setState({
+        learnButtonTextColor: "#F5F5F5",
+        learnBgColor: "transparent",
+        learnBorderColor: "#F5F5F5",
+      });
+    }
   }
 
   render() {
@@ -26,12 +97,46 @@ class Hero extends PureComponent {
               </div>
               <div
                 className="contact-button"
+                onMouseEnter={() => this.contactButtonOver()}
+                onMouseLeave={() => this.contactButtonLeave()}
+                style={{
+                  border: "1px solid",
+                  borderColor: `${this.state.contactBorderColor}`,
+                  backgroundColor: `${this.state.contactBgColor}`,
+                  borderRadius: "30px",
+                  display: "inline-block",
+                  color: `${this.state.contactButtonTextColor}`,
+                  textAlign: "center",
+                  margin: "0px 25px",
+                  padding: "8px 15px",
+                  fontSize: "1.563rem",
+                  fontWeight: "600",
+                  transition: "0.3s",
+                  cursor: "pointer",
+                }}
                 onClick={() => this.scrollTo("contact")}
               >
                 Contact Us
               </div>
               <div
                 className="learn-more-button"
+                onMouseEnter={() => this.learnButtonOver()}
+                onMouseLeave={() => this.learnButtonLeave()}
+                style={{
+                  border: "1px solid",
+                  borderColor: `${this.state.learnBorderColor}`,
+                  backgroundColor: `${this.state.learnBgColor}`,
+                  borderRadius: "30px",
+                  display: "inline-block",
+                  color: `${this.state.learnButtonTextColor}`,
+                  textAlign: "center",
+                  margin: "0px 25px",
+                  padding: "8px 15px",
+                  fontSize: "1.25rem",
+                  fontWeight: "600",
+                  transition: "0.3s",
+                  cursor: "pointer",
+                }}
                 onClick={() => this.scrollTo("about")}
               >
                 Learn More
